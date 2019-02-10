9b2646936f734a7518772790ff3ac4462a5c3455
alexe <alexe@W10SSD>
2019-02-10 18:39:36 +0300

    Корректное отображаение на странице

--- MS-Razor/Models/Movie.cs
+++ MS-Razor/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +12,13 @@ namespace MS_Razor.Models
         public int ID { get; set; } // Primary Key in DB
         public string Title { get; set; }
 
+        [Display(Name = "Дата выпуска")] // отображаемое имя 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; } // пользователю не требуется вводить сведения о времени 
         //в поле даты. Отображается только дата, а не время
         public string Genre { get; set; }
+
+        [Column(TypeName ="decimal(18, 2)")] // позволяет Entity Framework Core корректно сопоставить Price с валютой в базе данных
         public decimal Price { get; set; }
     }
 }
