5df095b4cc0b9b5c039fca57306e5b1aa4c570e5
alexe <alexe@W10SSD>
2019-02-10 19:38:12 +0300

    Добавил проверку

--- MS-Razor/Models/Movie.cs
+++ MS-Razor/Models/Movie.cs
@@ -10,17 +10,28 @@ namespace MS_Razor.Models
     public class Movie
     {
         public int ID { get; set; } // Primary Key in DB
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
-        [Display(Name = "Дата выпуска")] // отображаемое имя 
+        [Display(Name = "Дата Выпуска")] // отображаемое имя 
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; } // пользователю не требуется вводить сведения о времени 
-        //в поле даты. Отображается только дата, а не время
+                                                  //в поле даты. Отображается только дата, а не время
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")] // должна начинаться с одной или нескольких заглавных букв, после чего могут следовать ноль или несколько букв, двойных или одинарных кавычек, пробелов или дефисов
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
+        [Range(1, 100)] // ограничивает диапазон значений 
+        [DataType(DataType.Currency)]
         [Column(TypeName ="decimal(18, 2)")] // позволяет Entity Framework Core корректно сопоставить Price с валютой в базе данных
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required] // должно иметь значение
         public string Rating { get; set; }
     }
 }
